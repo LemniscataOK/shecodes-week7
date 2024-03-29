ec2eeba2fb96b06f672761aebb8bf22e80e29b5c
LemniscataOK <kolibanoksana@gmail.com>
2024-03-29 17:13:07 +0200

    added weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector ("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector ("#city");
+    let descriptionElement = document.querySelector ("#description");
+    let timeElement = document.querySelector ("#time");
+    let humidityElement = document.querySelector ("#humidity");
+    let windSpeedElement = document.querySelector ("#wind-speed");
+
 
     cityElement.innerHTML= response.data.city;
+    timeElement.innerHTML = "Tuesday 14:49";
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);}
 
 function searchCity(city) {let apiKey = "b2a5adcct04b33178913oc335f405433";let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units`
