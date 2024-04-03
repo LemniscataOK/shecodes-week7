f356d357851a2a66564ac745b0c3fb4e316ea014
LemniscataOK <kolibanoksana@gmail.com>
2024-04-03 17:30:55 +0300

    Last version - JS updated

--- src/index.js
+++ src/index.js
@@ -17,13 +17,10 @@ function refreshWeather(response){
 
 function searchCity(city) {let apiKey = "b2a5adcct04b33178913oc335f405433";let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units`
 axios.get(apiUrl).then(refreshWeather)}
-
 function handleSearchSubmit(event) {event.preventDefault(); 
     let searchInput = document.querySelector ("#search-form-input"); 
    
     searchCity(searchInput.value);}
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 searchCity("Kyiv");
